808e8a811658bcc611b6bf213e0832929a522ce4
NikitaWeW <nikita2510mart@gmail.com>
2023-08-11 20:04:57 +0300

    it's all working!

--- src/Engine/Camera.java
+++ src/Engine/Camera.java
@@ -5,8 +5,6 @@ import org.joml.*;
 import java.lang.Math;
 
 public class Camera {
-    //private Vector3f position = new Vector3f(0, 0, 0);
-    //private Vector3f rotation = new Vector3f(0, 0, 0);
     private Position position = new Position(0, 0, 0);
 
     public Position getPosition() {
